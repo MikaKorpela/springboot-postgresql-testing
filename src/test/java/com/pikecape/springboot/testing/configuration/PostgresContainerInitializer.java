3a4e38df6204479415cd558a897ef1605d7cdffa
Mika Korpela <mika.korpela@gofore.com>
2025-04-17 10:30:13 +0300

    Test configuration updated.

--- src/test/java/com/pikecape/springboot/testing/configuration/PostgresContainerInitializer.java
+++ src/test/java/com/pikecape/springboot/testing/configuration/PostgresContainerInitializer.java
@@ -6,33 +6,36 @@ import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 public class PostgresContainerInitializer implements
     ApplicationContextInitializer<ConfigurableApplicationContext>, AfterAllCallback {
 
-  private static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(
-      "postgres:14.1")
+  private static final PostgreSQLContainer postgreSqlContainer = new PostgreSQLContainer(
+      DockerImageName.parse("postgres:latest")
+  )
       .withDatabaseName("postgres")
       .withUsername("postgres")
       .withPassword("postgres");
 
-
   @Override
   public void initialize(ConfigurableApplicationContext applicationContext) {
-    postgreSQLContainer.start();
+    postgreSqlContainer.start();
 
     TestPropertyValues.of(
-        "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-        "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-        "spring.datasource.password=" + postgreSQLContainer.getPassword()
-    ).applyTo(applicationContext.getEnvironment());
+        "spring.datasource.url=" + postgreSqlContainer.getJdbcUrl(),
+        "spring.datasource.username=" + postgreSqlContainer.getUsername(),
+        "spring.datasource.password=" + postgreSqlContainer.getPassword()
+    )
+        .applyTo(applicationContext.getEnvironment());
   }
 
   @Override
   public void afterAll(ExtensionContext context) throws Exception {
-    if (postgreSQLContainer == null) {
+    if (postgreSqlContainer == null) {
       return;
     }
-    postgreSQLContainer.close();
+
+    postgreSqlContainer.close();
   }
 }
